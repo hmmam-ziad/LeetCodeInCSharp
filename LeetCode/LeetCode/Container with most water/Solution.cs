3ca33abe41c9bddf829f533dc9647e5f6bc1d6f8
hmmam-ziad <hmmamxxc@gmail.com>
2026-01-20 15:14:41 +0300

    Cotainer with most water

---
+++ LeetCode/LeetCode/Container with most water/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Container_with_most_water
+{
+    public class Solution
+    {
+        public int MaxArea(int[] heigth)
+        {
+            int maxArea = 0;
+            int l = 0;
+            int r = heigth.Length - 1;
+            while (l < r)
+            {
+                maxArea = Math.Max(maxArea, Math.Min(heigth[l], heigth[r]) * (r - l));
+                if (heigth[l] < heigth[r])
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+            return maxArea;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Program.cs
@@ -0,0 +1,5 @@
+﻿using LeetCode.Container_with_most_water;
+
+Solution solution = new Solution();
+int[] arr = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+Console.WriteLine(solution.MaxArea(arr));
