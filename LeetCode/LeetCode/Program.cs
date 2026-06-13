2df98592a5ed3ffed5074d46253f7a8f1e63d44b
hmmam-ziad <hmmamxxc@gmail.com>
2026-06-13 17:17:22 +0300

    Leet Code 189. Rotate Array

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -1,7 +1,13 @@
 ﻿
 
-using LeetCode.Revers_Nums;
+using LeetCode.Rotate_Array;
 
 Solution solution = new Solution();
+int[] nums = { -1, -100, 3, 99 };
+solution.Rotate(nums, 2);
+foreach (int num in nums)
+{
+    Console.Write(num);
+    Console.Write(" ");
 
-Console.WriteLine(string.Join(", ", solution.ReverseArr(new int[] { 1, 2, 3, 4, 5 })));
+}
---
+++ LeetCode/LeetCode/Rotate Array/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Rotate_Array
+{
+    /// <summary>
+    /// https://leetcode.com/problems/rotate-array/description/
+    /// </summary>
+    public class Solution
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            k = k % nums.Length;
+            Reverse(nums, 0, nums.Length - 1);
+            Reverse(nums, 0, k - 1);
+            Reverse(nums, k, nums.Length - 1);
+        }
+        public void Reverse(int[] nums, int left, int right)
+        {
+            while (left < right)
+            {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
