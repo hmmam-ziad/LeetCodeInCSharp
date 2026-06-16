b3d38dd08a0a6538b63872a3c96e43ed426c1c49
hmmam-ziad <hmmamxxc@gmail.com>
2026-06-16 18:27:27 +0300

    Leet Code 1299. Replace Elements with Greatest Element on Right Side

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -1,9 +1,9 @@
 ﻿
 
-using LeetCode.Duplicate_Zeros;
+using LeetCode.Replace_Elements_with_Greatest_Element_on_Right_Side;
 Solution solution = new Solution();
-int[] nums1 = new int[] { 0, 0, 0, 0, 0, 0, 0 };
-solution.DuplicateZeros(nums1);
+int[] nums1 = new int[] { 1, 400, 2 };
+solution.ReplaceElements(nums1);
 
 Console.Write("[");
 Console.Write(string.Join(", ", nums1));
---
+++ LeetCode/LeetCode/Replace Elements with Greatest Element on Right Side/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Replace_Elements_with_Greatest_Element_on_Right_Side
+{
+    /// <summary>
+    /// https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+    /// </summary>
+    public class Solution
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            int max = -1;
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                int newMax = Math.Max(max, arr[i]);
+                arr[i] = max;
+                max = newMax;
+            }
+
+            return arr;
+        }
+    }
+}
